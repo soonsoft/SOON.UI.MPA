a258fb7d8e3c804884660b9fd7196d6a543aad14
soonsoft <soonz@outlook.com>
2020-11-16 18:05:24 +0800

    update

--- src/common/screen/screen.js
+++ src/common/screen/screen.js
@@ -3,9 +3,10 @@ import { createLayout } from "./layout/layout";
 import "./components/operate-list";
 import "./components/message-list";
 
-import "./layout/module/layout-tab-panel";
 import "./layout/module/layout-ctrl-panel";
 import "./layout/module/layout-list-panel";
+import "./layout/module/layout-tab-panel";
+import "./layout/module/layout-tablist-panel";
 import "./layout/module/layout-message-panel";
 import "./layout/module/layout-chart-panel";
 
